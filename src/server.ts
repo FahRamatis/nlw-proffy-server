2b0dcef39527ad773f7de564a9b512f12ada8898
FahRamatis <fah.ramatis@gmail.com>
2020-08-08 12:38:10 -0300

    creating class

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
-app.get("/", (request, response) => response.json({ hello: "world" }));
+app.use(express.json());
+app.use(routes);
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Running on localhost:${PORT}`));
